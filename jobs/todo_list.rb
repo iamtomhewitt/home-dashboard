f311dbb43c7013de1f44f60d4ea0c7a4ef2780ff
Tom Hewitt <tommhewitt@gmail.com>
2019-09-15 10:50:27 +0100

    List css updates

--- jobs/todo_list.rb
+++ jobs/todo_list.rb
@@ -9,7 +9,7 @@ get_url         = 'http://dreamlo.com/lb/'+public_code+'/json'
 add_url         = 'http://dreamlo.com/lb/'+private_code+'/add/'
 delete_url      = 'http://dreamlo.com/lb/'+private_code+'/delete/'
 
-SCHEDULER.every "1m", :first_in => 0 do |job |
+SCHEDULER.every "15s", :first_in => 0 do |job |
     items = []
 
     uri = URI.parse(get_url)
@@ -22,6 +22,27 @@ SCHEDULER.every "1m", :first_in => 0 do |job |
         if entries.kind_of?(Array)
             entries.each do |child|
                 item_name = child['name']
+
+                # a very long stir <- the longest a string can be before it overflows
+                # todo replace the last the characters with ... if over a certain length
+
+                # TODO make it not open the link in a new tab
+                delete_command = """
+                    <a onclick='submit("""+item_name+""")'>Delete</a>
+                    <script type='text/javascript'>
+                        function submit(name) {
+                            alert('Hello there!');
+                            var xmlHttp = new XMLHttpRequest();
+                            xmlHttp.onreadystatechange = function() { 
+                                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                                    callback(xmlHttp.responseText);
+                            }
+                            xmlHttp.open('GET', "+delete_url+"name, true);
+                            xmlHttp.send(null);
+                        }
+                    </script>
+                """
+
                 items.push({
                     name: item_name,
                     deleteUrl: "<a href='"+delete_url+item_name+"' target='_blank'>Delete</a>"
