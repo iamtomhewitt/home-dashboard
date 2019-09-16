078669e77f513d08a48142407fcb5f94d337f116
Tom Hewitt <tommhewitt@gmail.com>
2019-09-16 15:42:46 +0100

    Update todo list to control text overflow

--- jobs/todo_list.rb
+++ jobs/todo_list.rb
@@ -22,36 +22,38 @@ SCHEDULER.every "15s", :first_in => 0 do |job |
         if entries.kind_of?(Array)
             entries.each do |child|
                 item_name = child['name']
-
-                # a very long stir <- the longest a string can be before it overflows
-                # todo replace the last the characters with ... if over a certain length
+                display_name = item_name
 
                 # TODO make it not open the link in a new tab
-                delete_command = """
-                    <a onclick='submit("""+item_name+""")'>Delete</a>
-                    <script type='text/javascript'>
-                        function submit(name) {
-                            alert('Hello there!');
-                            var xmlHttp = new XMLHttpRequest();
-                            xmlHttp.onreadystatechange = function() { 
-                                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                                    callback(xmlHttp.responseText);
-                            }
-                            xmlHttp.open('GET', "+delete_url+"name, true);
-                            xmlHttp.send(null);
-                        }
-                    </script>
-                """
+                # delete_command = """
+                #     <a onclick='submit("""+item_name+""")'>Delete</a>
+                #     <script type='text/javascript'>
+                #         function submit(name) {
+                #             alert('Hello there!');
+                #             var xmlHttp = new XMLHttpRequest();
+                #             xmlHttp.onreadystatechange = function() { 
+                #                 if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                #                     callback(xmlHttp.responseText);
+                #             }
+                #             xmlHttp.open('GET', "+delete_url+"name, true);
+                #             xmlHttp.send(null);
+                #         }
+                #     </script>
+                # """
+
+                if item_name.length >= 13
+                    display_name = item_name[0..13] + '...'
+                end
 
                 items.push({
-                    name: item_name,
+                    name: display_name,
                     deleteUrl: "<a href='"+delete_url+item_name+"' target='_blank'>Delete</a>"
                 });
             end
         else
             item_name = entries['name']
             items.push({
-                name: item_name,
+                name: display_name,
                 deleteUrl: "<a href='"+delete_url+item_name+"' target='_blank'>Delete</a>"
             });
         end
