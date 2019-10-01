a235393c53cd84be99c10ff7bf3b5fc92416f85c
Tom Hewitt <tommhewitt@gmail.com>
2019-10-01 19:30:34 +0100

    Add a Clock widget and a base class for all widgets

---
+++ Assets/Widgets/Clock/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : Widget
+{
+	[Space(15f)]
+	[SerializeField] private Text clockText;
+
+	private void Start()
+	{
+		this.Initialise();
+		InvokeRepeating("Run", 0f, 1f);
+	}
+
+	public override void Run()
+	{
+		clockText.text = DateTime.Now.ToString("HH:mm:ss");
+	}
+}
