942ccfed4de3ff2a7fcda81c69fc3cc2a5ec3c06
Tom Hewitt <tommhewitt@gmail.com>
2019-10-02 21:58:17 +0100

    Update Clock widget to show todays date

--- Assets/Widgets/Clock/Clock.cs
+++ Assets/Widgets/Clock/Clock.cs
@@ -5,16 +5,25 @@ using UnityEngine.UI;
 public class Clock : Widget
 {
 	[Space(15f)]
-	[SerializeField] private Text clockText;
+	public Text clockText;
+	public Text dateText;
+
+	private int oneDay = 86400;
 
 	private void Start()
 	{
 		this.Initialise();
 		InvokeRepeating("Run", 0f, this.repeatRate);
+		InvokeRepeating("UpdateDateText", 0f, oneDay);
 	}
 
 	public override void Run()
 	{
 		clockText.text = DateTime.Now.ToString("HH:mm:ss");
+	}
+
+	private void UpdateDateText()
+	{
+		dateText.text = DateTime.Now.ToString("dd MMMM yyyy");
 	}
 }
