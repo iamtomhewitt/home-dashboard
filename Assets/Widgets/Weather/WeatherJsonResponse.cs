48b03baaf2beb1e6f891095b74647acea2053199
Tom Hewitt <tommhewitt@gmail.com>
2019-10-04 12:47:36 +0100

    Add weather widget

---
+++ Assets/Widgets/Weather/WeatherJsonResponse.cs
@@ -0,0 +1,31 @@
+﻿[System.Serializable]
+public class WeatherJsonResponse
+{
+	public Currently currently;
+	public Daily daily;
+}
+
+[System.Serializable]
+public class Currently
+{
+	public string summary;
+	public string icon;
+	public double temperature;
+}
+
+[System.Serializable]
+public class Daily
+{
+	public string summary;
+	public Data[] data;
+}
+
+[System.Serializable]
+public class Data
+{
+	public long time;
+	public string summary;
+	public string icon;
+	public double temperatureHigh;
+	public double temperatureLow;
+}
