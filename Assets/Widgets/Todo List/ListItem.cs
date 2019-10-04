1c4ed03aded585b1375ae19124603144948627f3
Tom Hewitt <tommhewitt@gmail.com>
2019-10-04 15:41:00 +0100

    Add a todo list widget

---
+++ Assets/Widgets/Todo List/ListItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ListItem : MonoBehaviour
+{
+	public Text itemName;
+
+	public void SetName(string name)
+	{
+		this.itemName.text = name;
+	}
+
+	public void Delete()
+	{
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Widgets/Todo List/TodoList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TodoList : Widget
+{
+	[Space(15f)]
+	public GameObject addItemMenu;
+	public GameObject itemPrefab;
+	public Transform itemParent;
+	public Text statusText;
+
+	private void Start()
+	{
+		this.Initialise();
+		HideMenu();
+	}
+
+	public override void Run()
+	{
+		// Currently do nothing
+	}
+
+	// Called from a button
+	public void ShowMenu()
+	{
+		addItemMenu.SetActive(true);
+	}
+
+	// Called from a button
+	public void HideMenu()
+	{
+		addItemMenu.SetActive(false);
+	}
+
+	// Called from the add button on the menu that pops up
+	public void AddItem(InputField input)
+	{
+		GameObject item = Instantiate(itemPrefab, itemParent) as GameObject;
+		item.GetComponent<ListItem>().SetName(input.text);
+		statusText.text = "'" + input.text + "' added!";
+		input.text = "";
+		this.UpdateLastUpdatedText();
+	}
+}
+
