94104a8e36a2b678ab0a276cee7ecd55a73303ff
Tom Hewitt <tommhewitt@gmail.com>
2019-10-27 20:12:14 +0000

    Add saving and loading of food planner recipes to file so they are saved when the app is restarted

---
+++ Assets/RecipeCard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeCard : MonoBehaviour
+{
+	[SerializeField] private RecipeData recipeData;
+	[SerializeField] private InputField recipeInput;
+
+	public void SetRecipeName()
+	{
+		recipeData.recipeName = recipeInput.text;
+	}
+
+	public RecipeData GetRecipeData()
+	{
+		return recipeData;
+	}
+
+	public InputField GetRecipeInput()
+	{
+		return recipeInput;
+	}
+}
+
+[System.Serializable]
+public class RecipeData
+{
+	public string day;
+	public string recipeName;
+}
+
--- Assets/Widgets/FoodPlanner/FoodPlanner.cs
+++ Assets/Widgets/FoodPlanner/FoodPlanner.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
+using System.IO;
 
 public class FoodPlanner : Widget
 {
+	private RecipeCard[] recipes;
+	private string filePath;
+
 	private void Start()
 	{
+		filePath = Path.Combine(Application.persistentDataPath, "foodplanner.json");
+		recipes = FindObjectsOfType<RecipeCard>();
+
+		LoadRecipesFromFile();
+
 		this.Initialise();
 		InvokeRepeating("Run", 0f, ToSeconds());
 	}
 
 	public override void Run()
 	{
-		
+
+	}
+
+	/// <summary>
+	/// Saves the recipes to file.
+	/// </summary>
+	public void SaveToFile()
+	{
+		RecipeData[] data = new RecipeData[recipes.Length];
+
+		for (int i = 0; i < recipes.Length; i++)
+		{
+			data[i] = recipes[i].GetRecipeData();
+		}
+
+		string json = FoodPlannerJsonHelper.ToJson(data);
+
+		StreamWriter writer = new StreamWriter(filePath, false);
+		writer.WriteLine(json);
+		writer.Close();
+		print(filePath);
+	}
+
+	/// <summary>
+	/// Loads the recipes from files
+	/// </summary>
+	private void LoadRecipesFromFile()
+	{
+		string fileContent = File.ReadAllText(filePath);
+
+		RecipeData[] data = FoodPlannerJsonHelper.FromJson(fileContent);
+
+		for (int i = 0; i < data.Length; i++)
+		{
+			//print(data[i].day + ": " + data[i].recipeName);
+			recipes[i].GetRecipeInput().text = data[i].recipeName;
+			recipes[i].GetRecipeData().recipeName = data[i].recipeName;
+			recipes[i].GetRecipeData().day = data[i].day;
+		}
 	}
 }
