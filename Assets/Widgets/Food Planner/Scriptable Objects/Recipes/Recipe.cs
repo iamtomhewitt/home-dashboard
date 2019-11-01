029eaf48de84351d84f366a0da07e86f763d8279
Tom Hewitt <tommhewitt@gmail.com>
2019-11-01 15:48:09 +0000

    🥧 Add pie and peas recipe

--- Assets/Widgets/Food Planner/Scriptable Objects/Recipes/Recipe.cs
+++ Assets/Widgets/Food Planner/Scriptable Objects/Recipes/Recipe.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Recipe", menuName = "Recipe")]
 public class Recipe : ScriptableObject
 {
+    public int serves;
     public RecipeIngredient[] ingredients;
 
     [System.Serializable]
