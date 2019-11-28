730f486a949bc6e62d42cc4c8a7be3d2dbd3a6c5
Tom Hewitt <tommhewitt@gmail.com>
2019-11-28 12:00:36 +0000

    💻 Add a logger

---
+++ Assets/Scripts/Dialog/WidgetLogger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace Dialog
+{
+	public class WidgetLogger : Dialog
+	{
+		[SerializeField] private Text log;
+
+		public static WidgetLogger instance;
+
+		private DateTime lastDate;
+
+		private void Start()
+		{
+			instance = this;
+			lastDate = DateTime.Today;
+
+			InvokeRepeating("HouseKeep", 0f, 10800f); // Repeat every three hours
+
+			Hide();
+		}
+
+		public void Log(string message)
+		{
+			string date = DateTime.Now.ToString("dd/MMM/yy HH:mm");
+
+			log.text += date + " | " + message + "\n";
+			//print(date + " | " + message);
+		}
+
+		public void Log(Widget widget, string message)
+		{
+			string date = DateTime.Now.ToString("dd/MMM/yy HH:mm");
+			string messageFormatted = "<b><color=#" + ColorUtility.ToHtmlStringRGB(widget.GetWidgetColour()) + ">" + widget.GetWidgetTitle() + "</color></b>: " + message;
+
+			log.text += date + " | " + messageFormatted + "\n";
+			//print(date + " | " + messageFormatted);
+		}
+
+		private void HouseKeep()
+		{
+			if (lastDate != DateTime.Today)
+			{
+				log.text = "";
+			}
+			else
+			{
+				Log("<b>Widget Logger</b>: House keeping complete");
+			}
+
+			lastDate = DateTime.Today;
+		}
+	}
+}
---
+++ Assets/Scripts/Version.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Text>().text = "Version: " + Application.version;
+    }
+}
