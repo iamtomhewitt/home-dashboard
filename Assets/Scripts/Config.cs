2386e065d24e473a428efc8e054a9933d6143c80
Tom Hewitt <tommhewitt@gmail.com>
2019-12-14 16:10:17 +0000

    💻 Get the dashboard to manage its own config if one has not been supplied

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using SimpleJSON;
+using System.IO;
 
 /// <summary>
 /// Access config as JSON via this component. <para/>
@@ -9,6 +10,7 @@ using SimpleJSON;
 public class Config : MonoBehaviour
 {
 	[SerializeField] private TextAsset configFile;
+	[SerializeField] private TextAsset configFileTemplate;
 
 	public static Config instance;
 
@@ -17,11 +19,55 @@ public class Config : MonoBehaviour
 	private void Awake()
 	{
 		instance = this;
-		root = JSON.Parse(configFile.text);
+		string filePath = Application.persistentDataPath + "/config.json";
+
+		if (configFile != null)
+		{
+			root = JSON.Parse(configFile.text);
+		}
+		else if (File.Exists(filePath))
+		{
+			print("A config file has already been created at: " + filePath + ", using that one.");
+			string contents = GetFileContents(filePath);
+			root = JSON.Parse(contents);
+		}
+		else
+		{
+			print("A config file has not been specified, creating one.");
+			CreateNewFile(filePath);
+			root = JSON.Parse(configFileTemplate.text);
+		}
 	}
 
 	public JSONNode GetConfig()
 	{
 		return root;
+	}
+
+	public void Replace(JSONNode key, string value)
+	{
+		key.Value = value;
+		SaveToFile();
+	}
+
+	public void SaveToFile()
+	{
+		string filePath = Application.persistentDataPath + "/config.json";
+		StreamWriter writer = new StreamWriter(filePath, false);
+		writer.Write(root.ToString());
+		writer.Close();
+	}
+
+	private void CreateNewFile(string filePath)
+	{
+		StreamWriter writer = new StreamWriter(filePath, true);
+		writer.Write(configFileTemplate.text);
+		writer.Close();
+	}
+
+	private string GetFileContents(string filePath)
+	{
+		StreamReader reader = new StreamReader(filePath);
+		return reader.ReadToEnd();
 	}
 }
