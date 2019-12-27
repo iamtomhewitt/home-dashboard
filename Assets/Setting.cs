e67e2783b3bfb0b8ff621660c04f04dd39f18214
Tom Hewitt <tommhewitt@gmail.com>
2019-12-27 15:13:42 +0000

    💻 Add a settings dialog and a settings object to gather all the indivudal settings

---
+++ Assets/Setting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A key lavel and value input field on the settings page.
+/// </summary>
+public class Setting : MonoBehaviour
+{
+	[SerializeField] private string[] keyTree;
+	[SerializeField] private InputField value;
+
+	/// <summary>
+	/// Returns the key of the value of the tree.
+	/// E.g. apiKeys->googleCalendars->someone@gmail.com would return the key someone@gmail.com
+	/// </summary>
+	/// <returns></returns>
+	public string GetKey()
+	{
+		return keyTree[keyTree.Length -1];
+	}
+
+	public string GetValue()
+	{
+		return value.text;
+	}
+}
---
+++ Assets/SettingsDialog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialog
+{
+	public class SettingsDialog : Dialog
+	{
+		/// <summary>
+		/// Called from a Unity button, saves all the settings objects to the config file.
+		/// </summary>
+		public void Save()
+		{
+			Config config = Config.instance;
+			List<Setting> settings = new List<Setting>(FindObjectsOfType<Setting>());
+
+			foreach (Setting setting in settings)
+			{
+				print("Key: " + setting.GetKey());
+				print("Value: " + setting.GetValue());
+			}
+		}
+	}
+}
