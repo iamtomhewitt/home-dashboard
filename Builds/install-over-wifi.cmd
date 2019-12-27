485a2d7bb7d80da4c26e06b1c9d92cf1b9bfb034
Tom Hewitt <tommhewitt@gmail.com>
2019-12-27 16:34:14 +0000

    💻 Add command to start the app on the tablet

--- Builds/install-over-wifi.cmd
+++ Builds/install-over-wifi.cmd
@@ -5,11 +5,14 @@ set /p device_ip=Device IP Address:
 REM Connect
 adb connect %device_ip%:5555
 
-REM REM Uninstall
-REM adb uninstall com.TomHewitt.HomeDashboard
+REM Uninstall
+adb uninstall com.TomHewitt.HomeDashboard
 
-REM REM Install
-REM adb install "Home Dashboard".apk
+REM Install
+adb install "Home Dashboard".apk
+
+REM Start the app on the tablet
+adb shell monkey -p com.TomHewitt.HomeDashboard -c android.intent.category.LAUNCHER 1
 
 REM Disconnect
 adb disconnect %device_ip%:5555
