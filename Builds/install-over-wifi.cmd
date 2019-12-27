256b7cb932b7e0530cecb6989d3c069993335585
Tom Hewitt <tommhewitt@gmail.com>
2019-12-27 16:45:40 +0000

    📝 Script updates

--- Builds/install-over-wifi.cmd
+++ Builds/install-over-wifi.cmd
@@ -3,13 +3,21 @@ REM Get IP address
 set /p device_ip=Device IP Address: 
 
 REM Connect
+echo.
+echo Connecting to device
 adb connect %device_ip%:5555
 
 REM Install
+echo.
+echo Installing dashboard
 adb install -r "Home Dashboard".apk
 
 REM Start the app on the tablet
+echo.
+echo Restarting dashboard
 adb shell monkey -p com.TomHewitt.HomeDashboard -c android.intent.category.LAUNCHER 1
 
 REM Disconnect
+echo.
+echo Disconnecting from device
 adb disconnect %device_ip%:5555
