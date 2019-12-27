c3e26a628fa266db361d022fa57e42640c1aa1b2
Tom Hewitt <tommhewitt@gmail.com>
2019-12-27 16:37:26 +0000

    🐛 Change install command so no cache/data is lost

--- Builds/install-over-wifi.cmd
+++ Builds/install-over-wifi.cmd
@@ -5,11 +5,8 @@ set /p device_ip=Device IP Address:
 REM Connect
 adb connect %device_ip%:5555
 
-REM Uninstall
-adb uninstall com.TomHewitt.HomeDashboard
-
 REM Install
-adb install "Home Dashboard".apk
+adb install -r "Home Dashboard".apk
 
 REM Start the app on the tablet
 adb shell monkey -p com.TomHewitt.HomeDashboard -c android.intent.category.LAUNCHER 1
