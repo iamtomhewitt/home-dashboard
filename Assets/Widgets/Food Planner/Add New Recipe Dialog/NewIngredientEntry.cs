889f2dc3e9867f0977a08d064fa6165c97cd737a
Tom Hewitt <tommhewitt@gmail.com>
2020-01-06 22:16:26 +0000

    💻 Create a new ingredient prefab

---
+++ Assets/Widgets/Food Planner/Add New Recipe Dialog/NewIngredientEntry.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewIngredientEntry : MonoBehaviour
+{
+	[SerializeField] private InputField ingredientName;
+	[SerializeField] private InputField quantity;
+	[SerializeField] private Dropdown type;
+	[SerializeField] private Dropdown weight;
+}
