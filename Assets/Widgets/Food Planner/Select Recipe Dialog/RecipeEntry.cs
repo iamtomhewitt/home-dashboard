46a0dfe656edc6e94c9f8a105dc9319cb9a13eee
Tom Hewitt <tommhewitt@gmail.com>
2020-01-06 21:51:50 +0000

    💻 Add an add new recipe dialog

---
+++ Assets/Widgets/Food Planner/Select Recipe Dialog/RecipeEntry.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// The recipes retrieved from Heroku shown on the recipe selection dialog.
+/// </summary>
+public class RecipeEntry : MonoBehaviour
+{
+	[SerializeField] private Text text;
+
+	public void SetText(string message)
+	{
+		text.text = message;
+	}
+
+	public string GetText()
+	{
+		return text.text;
+	}
+}
