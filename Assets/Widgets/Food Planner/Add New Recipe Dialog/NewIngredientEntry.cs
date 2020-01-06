96e2cd57c8bac57264d426c15d0c643cb8faf3b5
Tom Hewitt <tommhewitt@gmail.com>
2020-01-06 22:42:56 +0000

    💻 Change type to category for new ingredients

--- Assets/Widgets/Food Planner/Add New Recipe Dialog/NewIngredientEntry.cs
+++ Assets/Widgets/Food Planner/Add New Recipe Dialog/NewIngredientEntry.cs
@@ -5,6 +5,26 @@ public class NewIngredientEntry : MonoBehaviour
 {
 	[SerializeField] private InputField ingredientName;
 	[SerializeField] private InputField quantity;
-	[SerializeField] private Dropdown type;
+	[SerializeField] private Dropdown category;
 	[SerializeField] private Dropdown weight;
+
+	public string GetIngredientName()
+	{
+		return ingredientName.text;
+	}
+
+	public string GetQuantity()
+	{
+		return quantity.text;
+	}
+
+	public string GetCategory()
+	{
+		return category.itemText.text;
+	}
+
+	public string GetWeight()
+	{
+		return weight.itemText.text;
+	}
 }
