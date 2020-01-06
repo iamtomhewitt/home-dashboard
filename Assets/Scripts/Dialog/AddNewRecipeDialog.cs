63f280b2a9d9fc41326ea6b23eae02d0424ae968
Tom Hewitt <tommhewitt@gmail.com>
2020-01-06 22:17:35 +0000

    💻 Add ability to add new ingredient to dialog

---
+++ Assets/Scripts/Dialog/AddNewRecipeDialog.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Dialog
+{
+	public class AddNewRecipeDialog : MonoBehaviour
+	{
+		[SerializeField] private GameObject newIngredientEntry;
+		[SerializeField] private Transform newIngredientsContent;
+
+		/// <summary>
+		/// Called from a Unity button to add a new ingredient to the add new recipe dialog.
+		/// </summary>
+		public void AddNewIngredientEntry()
+		{
+			Instantiate(newIngredientEntry, newIngredientsContent);
+		}
+	}
+}
