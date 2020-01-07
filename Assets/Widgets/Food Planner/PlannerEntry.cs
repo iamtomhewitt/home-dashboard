63b13af0f78be8796079f65913cb90238fbf36d7
Tom Hewitt <tommhewitt@gmail.com>
2020-01-07 22:54:38 +0000

    💻 Add the recipe to the heroku planner when a recipe is selected

--- Assets/Widgets/Food Planner/PlannerEntry.cs
+++ Assets/Widgets/Food Planner/PlannerEntry.cs
@@ -2,13 +2,27 @@
 using UnityEngine.UI;
 using System.Collections;
 using Dialog;
-using System;
+using UnityEngine.Networking;
+using System.Text;
+using SimpleJSON;
 
 namespace FoodPlannerWidget
 {
 	public class PlannerEntry : MonoBehaviour
 	{
+		[SerializeField] private Day day;
+		[SerializeField] private Text dayText;
 		[SerializeField] private Text recipe;
+
+		private void Start()
+		{
+			string label = "";
+			foreach (char c in day.ToString().Substring(0, 3).ToUpper())
+			{
+				label += c + "\n";
+			}
+			dayText.text = label;
+		}
 
 		/// <summary>
 		/// Called from a Unity button when the planner entry is clicked on.
@@ -33,8 +47,27 @@ namespace FoodPlannerWidget
 			if (dialog.GetResult() == DialogResult.FINISHED)
 			{
 				recipe.text = !string.IsNullOrEmpty(dialog.GetSelectedRecipe()) ? dialog.GetSelectedRecipe() : dialog.GetFreeTextRecipeName();
+
+				// Now update the planner online
+				string url = "https://home-dashboard-recipe-manager.herokuapp.com/planner/add";
+				JSONObject json = new JSONObject();
+				json.Add("recipe", recipe.text);
+				json.Add("day", day.ToString());
+
+				UnityWebRequest request = UnityWebRequest.Post(url, "POST");
+				byte[] body = Encoding.UTF8.GetBytes(json.ToString());
+
+				request.uploadHandler = new UploadHandlerRaw(body);
+				request.downloadHandler = new DownloadHandlerBuffer();
+				request.SetRequestHeader("Content-Type", "application/json");
+
+				yield return request.SendWebRequest();
+				print(request.downloadHandler.text);
+
 				yield break;
 			}
 		}
+
+		private enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
 	}
 }
