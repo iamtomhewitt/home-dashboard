9abb8283c24265c051b6fbef0b1aeb37a1907637
Tom Hewitt <tommhewitt@gmail.com>
2020-01-18 12:59:40 +0000

    🎨 Add a splitwise widget

---
+++ Assets/Widgets/Splitwise/Splitwise.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Splitwise : Widget
+{
+	[Header("Splitwise Settings")]
+	[SerializeField] private Text text;
+	
+	private void Start()
+    {
+        
+    }
+
+    public override void Run()
+	{
+
+	}
+}
