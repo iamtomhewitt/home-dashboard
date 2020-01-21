a5138a2c967b5f6c19d4ceb9f6132610aeefee64
Tom Hewitt <tommhewitt@gmail.com>
2020-01-21 13:38:16 +0000

    🎨 Add in ability to change the bbc news colour

--- Assets/Widgets/BBC News/BBCNews.cs
+++ Assets/Widgets/BBC News/BBCNews.cs
@@ -38,6 +38,10 @@ namespace BBCNews
 		{
 			this.ReloadConfig();
 			StartCoroutine(RequestHeadlines());
+
+			this.SetWidgetColour(GetWidgetColour());
+			entry.SetTextColour(GetTextColour());
+
 			this.UpdateLastUpdatedText();
 		}
 
