10a63d25605cc5ede19a3eb1898e5be712fa5509
Tom Hewitt <tommhewitt@gmail.com>
2020-01-21 13:30:56 +0000

    💻 Add a utils class for lightening and darkening colours

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Utils
+{
+	public static Color Lighten(Color colour)
+	{
+		return Color.Lerp(colour, Color.white, 0.3f);
+	}
+
+	public static Color Darken(Color colour)
+	{
+		return Color.Lerp(colour, Color.black, 0.3f);
+	}
+}
