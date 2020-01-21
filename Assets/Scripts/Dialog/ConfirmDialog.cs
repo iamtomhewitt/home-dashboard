1f4398ca5448b6a0fe0db44418a40eba0d4b7201
Tom Hewitt <tommhewitt@gmail.com>
2020-01-21 16:07:01 +0000

    🎨 Apply colours retrieved from config to ConfirmDialog

--- Assets/Scripts/Dialog/ConfirmDialog.cs
+++ Assets/Scripts/Dialog/ConfirmDialog.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using SimpleJSON;
 
 namespace Dialog
 {
 	public class ConfirmDialog : Dialog
 	{
+		[Header("Confirm Settings")]
 		[SerializeField] private Text infoText;
+		[SerializeField] private Button yesButton;
+		[SerializeField] private Button noButton;
 
 		private void Start()
 		{
 			Hide();
+		}
+
+		public void ApplyColours()
+		{
+			JSONNode config = Config.instance.GetDialogConfig()["confirm"];
+			Color mainColour = Utils.ToColour(config["mainColour"]);
+			Color textColour = Utils.ToColour(config["textColour"]);
+
+			SetDialogTitleColour(textColour);
+			SetHideButtonColour(mainColour, textColour);
+
+			yesButton.GetComponent<Image>().color = mainColour;
+			yesButton.GetComponentInChildren<Text>().color = textColour;
+
+			noButton.GetComponent<Image>().color = mainColour;
+			noButton.GetComponentInChildren<Text>().color = textColour;
 		}
 
 		public void SetInfoMessage(string message)
