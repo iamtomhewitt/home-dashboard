28e64bf6d218706afeca36faeb4b8ae530d05fa9
Tom Hewitt <tommhewitt@gmail.com>
2020-02-13 16:29:13 +0000

    💻 Create json bodies for adding a recipe

--- Assets/Scripts/Json/JsonBody.cs
+++ Assets/Scripts/Json/JsonBody.cs
@@ -12,5 +12,24 @@ namespace Json
 			json.Add("apiKey", apiKey);
 			return json;
 		}
+
+		public static JSONObject RECIPE_ADD(string name, JSONArray ingredients, string apiKey)
+		{
+			JSONObject json = new JSONObject();
+			json.Add("name", name);
+			json.Add("ingredients", ingredients);
+			json.Add("apiKey", apiKey);
+			return json;
+		}
+
+		public static JSONObject RECIPE_INGREDIENT(string name, double amount, string weight, string category)
+		{
+			JSONObject json = new JSONObject();
+			json.Add("name", name);
+			json.Add("amount", amount);
+			json.Add("weight", weight);
+			json.Add("category", category);
+			return json;
+		}
 	}
 }
