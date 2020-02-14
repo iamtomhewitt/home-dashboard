4646f4f9b9751025587c5fde632a238a406df77d
Tom Hewitt <tommhewitt@gmail.com>
2020-02-14 13:24:49 +0000

    💻 Reformat Postman class to accept JSONBody or string for the body parameter

--- Assets/Scripts/Requests/Postman.cs
+++ Assets/Scripts/Requests/Postman.cs
@@ -15,41 +15,24 @@ namespace Requests
 			return UnityWebRequest.Get(url);
 		}
 
-		public static UnityWebRequest CreateGetRequest(string url, JSONObject body)
-		{
-			UnityWebRequest request = UnityWebRequest.Get(url);
-			byte[] bytes = Encoding.UTF8.GetBytes(body.ToString());
-
-			request.uploadHandler = new UploadHandlerRaw(bytes);
-			request.downloadHandler = new DownloadHandlerBuffer();
-			request.SetRequestHeader("Content-Type", "application/json");
-
-			return request;
-		}
-
 		public static UnityWebRequest CreatePostRequest(string url, JSONObject body)
 		{
+			return PostRequest(url, body.ToString());
+		}
+
+		public static UnityWebRequest CreatePostRequest(string url, string body)
+		{
+			return PostRequest(url, body);
+		}
+
+		private static UnityWebRequest PostRequest(string url, string jsonBodyAsString)
+		{
 			UnityWebRequest request = UnityWebRequest.Post(url, "POST");
-			byte[] bytes = Encoding.UTF8.GetBytes(body.ToString());
+			byte[] bytes = Encoding.UTF8.GetBytes(jsonBodyAsString);
 
 			request.uploadHandler = new UploadHandlerRaw(bytes);
 			request.downloadHandler = new DownloadHandlerBuffer();
 			request.SetRequestHeader("Content-Type", "application/json");
-
-			return request;
-		}
-
-		public static UnityWebRequest CreateTodoistRequest(string url, string json, string apiKey, string uuid)
-		{
-			UnityWebRequest request = new UnityWebRequest(url, "POST");
-			byte[] jsonToSend = new UTF8Encoding().GetBytes(json);
-
-			request.uploadHandler = new UploadHandlerRaw(jsonToSend);
-			request.downloadHandler = new DownloadHandlerBuffer();
-
-			request.SetRequestHeader("Content-Type", "application/json");
-			request.SetRequestHeader("Authorization", "Bearer " + apiKey);
-			request.SetRequestHeader("X-Request-Id", uuid);
 
 			return request;
 		}
