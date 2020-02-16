3cde0d422dd887fade5c29d38051cfc08cf9836a
Tom Hewitt <tommhewitt@gmail.com>
2020-02-16 19:53:06 +0000

    🐛 Actually add the recipe to the planner

--- Assets/Scripts/Json/JsonBody.cs
+++ Assets/Scripts/Json/JsonBody.cs
@@ -11,7 +11,7 @@ namespace SimpleJSON
 		public static JSONObject AddToPlanner(string recipe, string day, string apiKey)
 		{
 			JSONObject json = new JSONObject();
-			json.Add("recipe", " ");
+			json.Add("recipe", recipe);
 			json.Add("day", day);
 			json.Add("apiKey", apiKey);
 			return json;
