b8f0fee9c0d597280ca95fc6d043d655c8d1e301
Tom Hewitt <tommhewitt@gmail.com>
2020-02-21 13:33:46 +0000

    💻 Add journey planner endpoint

--- Assets/Widgets/Journey Planner/JourneyPlanner.cs
+++ Assets/Widgets/Journey Planner/JourneyPlanner.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
+using SimpleJSON;
 
 public class JourneyPlanner : Widget
 {
+	private JSONNode config;
+
+	private string startPoint;
+	private string endPoint;
+	private string apiKey;
+
 	public override void ReloadConfig()
 	{
-		throw new System.NotImplementedException();
+		config = Config.instance.GetWidgetConfig()[this.GetWidgetConfigKey()];
+		startPoint = config["startPoint"];
+		endPoint = config["endPoint"];
+		apiKey = config["apiKey"];
 	}
 
 	public override void Run()
 	{
-		throw new System.NotImplementedException();
+		this.ReloadConfig();
 	}
 }
