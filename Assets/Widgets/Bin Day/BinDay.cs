6d0f873e6018f1d6a5875dffd536e6d2ec5e3073
Tom Hewitt <tommhewitt@gmail.com>
2020-02-21 10:53:25 +0000

    💻 Strip back the bin day widget

--- Assets/Widgets/Bin Day/BinDay.cs
+++ Assets/Widgets/Bin Day/BinDay.cs
@@ -8,63 +8,28 @@ namespace Bins
 	public class BinDay : Widget
 	{
 		[Header("Bin Day Settings")]
-		[SerializeField] private Color greenBinColour;
-		[SerializeField] private Color blackBinColour;
 		[SerializeField] private Color noBinColour;
 
 		[SerializeField] private Text text;
 		[SerializeField] private Image logo;
 
-		private DateTime firstGreenBinDay;
-		private DateTime firstBlackBinDay;
-
-		private int repeatRateInDays;
+		private JSONArray bins;
 
 		public override void ReloadConfig()
 		{
 			JSONNode config = Config.instance.GetWidgetConfig()[this.GetWidgetConfigKey()];
-			firstGreenBinDay = DateTime.ParseExact(config["firstGreenBin"], "dd-MM-yyyy", null);
-			firstBlackBinDay = DateTime.ParseExact(config["firstBlackBin"], "dd-MM-yyyy", null);
-			repeatRateInDays = config["repeatRateInDays"];
-			greenBinColour = Colours.ToColour(config["greenBinColour"]);
-			blackBinColour = Colours.ToColour(config["blackBinColour"]);
 			noBinColour = Colours.ToColour(config["noBinColour"]);
+			bins = config["bins"].AsArray;
 		}
 
 		public override void Run()
 		{
 			this.ReloadConfig();
 
-			DateTime today = DateTime.Today;
-			DateTime tomorrow = today.AddDays(1);
-
-			DateTime lastGreenBinDay = GetLastBinDate(firstGreenBinDay);
-			DateTime lastBlackBinDay = GetLastBinDate(firstBlackBinDay);
-			DateTime nextGreenBinDay = GetNextBinDate(lastGreenBinDay);
-			DateTime nextBlackBinDay = GetNextBinDate(lastBlackBinDay);
-
-			if (today == nextBlackBinDay || today == lastBlackBinDay)
+			foreach(JSONNode bin in bins)
 			{
-				Display("Black bin today!", FontStyle.Bold, blackBinColour, Colours.Lighten(blackBinColour));
+				print (bin);
 			}
-			else if (today == nextGreenBinDay || today == lastGreenBinDay)
-			{
-				Display("Green bin today!", FontStyle.Bold, greenBinColour, Colours.Darken(greenBinColour));
-			}
-			else if (tomorrow == nextBlackBinDay || tomorrow == lastBlackBinDay)
-			{
-				Display("Black bin tomorrow!", FontStyle.Normal, blackBinColour, Colours.Lighten(blackBinColour));
-			}
-			else if (tomorrow == nextGreenBinDay || tomorrow == lastGreenBinDay)
-			{
-				Display("Green bin tomorrow!", FontStyle.Normal, greenBinColour, Colours.Darken(greenBinColour));
-			}
-			else
-			{
-				Display("No bins today!", FontStyle.Normal, noBinColour, Colours.Darken(noBinColour));
-			}
-
-			this.UpdateLastUpdatedText();
 		}
 
 		private void Display(string message, FontStyle style, Color widgetColour, Color logoColour)
@@ -76,7 +41,7 @@ namespace Bins
 			this.SetWidgetColour(widgetColour);
 		}
 
-		private DateTime GetLastBinDate(DateTime firstBinDate)
+		private DateTime GetLastBinDate(DateTime firstBinDate, int repeatRateInDays)
 		{
 			DateTime today = DateTime.Today;
 			int days = (int)(today - firstBinDate).TotalDays;
@@ -84,7 +49,7 @@ namespace Bins
 			return today.AddDays(-remainder);
 		}
 
-		private DateTime GetNextBinDate(DateTime lastBinDate)
+		private DateTime GetNextBinDate(DateTime lastBinDate, int repeatRateInDays)
 		{
 			return lastBinDate.AddDays(repeatRateInDays);
 		}
