15cc2a0a399bb6d4ff22301698880c2a08b9ee69
Tom Hewitt <tommhewitt@gmail.com>
2020-02-21 14:21:15 +0000

    💻 Retrieve times for multiple journeys

--- Assets/Widgets/Journey Planner/JourneyPlanner.cs
+++ Assets/Widgets/Journey Planner/JourneyPlanner.cs
@@ -7,17 +7,14 @@ using SimpleJSON;
 public class JourneyPlanner : Widget
 {
 	private JSONNode config;
-
-	private string startPoint;
-	private string endPoint;
+	private JSONNode journeys;
 	private string apiKey;
 
 	public override void ReloadConfig()
 	{
 		config = Config.instance.GetWidgetConfig()[this.GetWidgetConfigKey()];
-		startPoint = config["startPoint"];
-		endPoint = config["endPoint"];
 		apiKey = config["apiKey"];
+		journeys = config["journeys"];
 	}
 
 	public override void Run()
@@ -31,17 +28,24 @@ public class JourneyPlanner : Widget
 
 	private IEnumerator RunRoutine()
 	{
-		UnityWebRequest request = Postman.CreateGetRequest(Endpoints.JOURNEY_PLANNER(startPoint, endPoint, apiKey));
-		yield return request.SendWebRequest();
-		JSONNode json = JSON.Parse(request.downloadHandler.text);
+		foreach (JSONNode journey in journeys)
+		{
+			UnityWebRequest request = Postman.CreateGetRequest(Endpoints.JOURNEY_PLANNER(journey["startPoint"], journey["endPoint"], apiKey));
+			yield return request.SendWebRequest();
+			JSONNode json = JSON.Parse(request.downloadHandler.text);
 
-		int durationNormal = json["resourceSets"][0]["resources"][0]["travelDuration"];
-		int durationTraffic = json["resourceSets"][0]["resources"][0]["travelDurationTraffic"];
+			int durationNormal = json["resourceSets"][0]["resources"][0]["travelDuration"];
+			int durationTraffic = json["resourceSets"][0]["resources"][0]["travelDurationTraffic"];
 
-		print(ConvertToTimeString(durationNormal));
-		print(ConvertToTimeString(durationTraffic));
+			print(journey["name"]);
+			print(ConvertToTimeString(durationNormal));
+			print(ConvertToTimeString(durationTraffic));
+		}
 	}
 
+	/// <summary>
+	/// Passing 3468 seconds will return "57 minutes"
+	/// </summary>
 	private string ConvertToTimeString(int durationInSeconds)
 	{
 		string timeString = "";
