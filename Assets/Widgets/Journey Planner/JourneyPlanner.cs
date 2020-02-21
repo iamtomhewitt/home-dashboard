a8e048bf1950fa549ff349f0376f6c73faa2c80c
Tom Hewitt <tommhewitt@gmail.com>
2020-02-21 13:21:10 +0000

    💻 Add a journey planner widget

---
+++ Assets/Widgets/Journey Planner/JourneyPlanner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class JourneyPlanner : Widget
+{
+	public override void ReloadConfig()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override void Run()
+	{
+		throw new System.NotImplementedException();
+	}
+}
