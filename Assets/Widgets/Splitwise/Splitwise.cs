7693043fa9230000fc374aee5798dbb7a4f0543a
Tom Hewitt <tommhewitt@gmail.com>
2020-02-24 19:57:28 +0000

    💎 Show service unavailable if there is a 503

--- Assets/Widgets/Splitwise/Splitwise.cs
+++ Assets/Widgets/Splitwise/Splitwise.cs
@@ -8,47 +8,51 @@ using Dialog;
 
 public class Splitwise : Widget
 {
-	[Header("Splitwise Settings")]
-	[SerializeField] private Text groupName;
-	[SerializeField] private Text people;
-	[SerializeField] private Text amount;
-	[SerializeField] private Text allSettledUp;
+    [Header("Splitwise Settings")]
+    [SerializeField] private Text groupName;
+    [SerializeField] private Text people;
+    [SerializeField] private Text amount;
+    [SerializeField] private Text allSettledUp;
 
-	private string groupId;
-	private string apiKey;
+    private string groupId;
+    private string apiKey;
 
-	public override void ReloadConfig()
-	{
-		JSONNode config = Config.instance.GetWidgetConfig()[this.GetWidgetConfigKey()];
-		groupId = config["groupId"];
-		apiKey = config["apiKey"];
-	}
+    public override void ReloadConfig()
+    {
+        JSONNode config = Config.instance.GetWidgetConfig()[this.GetWidgetConfigKey()];
+        groupId = config["groupId"];
+        apiKey = config["apiKey"];
+    }
 
     public override void Run()
-	{
-		this.ReloadConfig();
-		StartCoroutine(RunRoutine());
-		this.UpdateLastUpdatedText();
-	}
+    {
+        this.ReloadConfig();
+        StartCoroutine(RunRoutine());
+        this.UpdateLastUpdatedText();
+    }
 
-	private IEnumerator RunRoutine()
-	{
-		UnityWebRequest request = Postman.CreateGetRequest(Endpoints.SPLITWISE(groupId, apiKey));
-		yield return request.SendWebRequest();
+    private IEnumerator RunRoutine()
+    {
+        UnityWebRequest request = Postman.CreateGetRequest(Endpoints.SPLITWISE(groupId, apiKey));
+        yield return request.SendWebRequest();
 
-		bool ok = request.error == null ? true : false;
-		if (!ok)
-		{
-			WidgetLogger.instance.Log(this, "Error: " + request.error);
-			yield break;
-		}
+        bool ok = request.error == null ? true : false;
+        if (!ok)
+        {
+            WidgetLogger.instance.Log(this, "Error: " + request.error);
+            people.text = request.responseCode == 503 ? "Service Unavailable" : request.error;
+            groupName.text = "";
+            allSettledUp.text = "";
+            amount.text = "";
+            yield break;
+        }
 
-		JSONNode json = JSON.Parse(request.downloadHandler.text);
-		JSONNode exp = json["expenses"][0];
+        JSONNode json = JSON.Parse(request.downloadHandler.text);
+        JSONNode exp = json["expenses"][0];
 
-		groupName.text = json["groupName"];
-		people.text 		= exp == null ? "" : exp["who"] + " owes " + exp["owes"];
-		allSettledUp.text 	= exp == null ? "All settled up!" : "";
-		amount.text = exp["amount"];
-	}
+        groupName.text = json["groupName"];
+        people.text = exp == null ? "" : exp["who"] + " owes " + exp["owes"];
+        allSettledUp.text = exp == null ? "All settled up!" : "";
+        amount.text = exp["amount"];
+    }
 }
