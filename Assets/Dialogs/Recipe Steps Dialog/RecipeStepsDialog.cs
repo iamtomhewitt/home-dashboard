bd985ce26367da31db566fddeb766945a4d29541
Tom Hewitt <tommhewitt@gmail.com>
2020-05-10 23:04:05 +0100

    💻 Add a recipe steps dialog

---
+++ Assets/Dialogs/Recipe Steps Dialog/RecipeStepsDialog.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Dialog
+{
+    public class RecipeStepsDialog : PopupDialog
+    {
+        public override void ApplyAdditionalColours(Color mainColour, Color textColour)
+        {
+            // Nothing to do
+        }
+    }
+}
