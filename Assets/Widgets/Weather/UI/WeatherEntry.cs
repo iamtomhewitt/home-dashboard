8f519b18546db476fca0bce46180a082c6a890f5
Tom Hewitt <tommhewitt@gmail.com>
2020-05-20 17:45:49 +0100

    🎨 Better alignment of weather icons

--- Assets/Widgets/Weather/UI/WeatherEntry.cs
+++ Assets/Widgets/Weather/UI/WeatherEntry.cs
@@ -4,46 +4,74 @@ using TMPro;
 
 namespace WeatherForecast
 {
-    public class WeatherEntry : MonoBehaviour
-    {
-        [SerializeField] private TMP_Text icon;
-        [SerializeField] private TMP_Text day;
-        [SerializeField] private TMP_Text temperature;
+	public class WeatherEntry : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text icon;
+		[SerializeField] private TMP_Text day;
+		[SerializeField] private TMP_Text temperature;
 
-		private List<string> outOfLineCharacters = new List<string> {"1", "c", "K", "W", "I", ","};
+		private List<string> outOfLineCharacters = new List<string> { "1", "c", "K", "W", "I", "," };
 
-        public void SetDayText(string s)
-        {
-            day.text = s;
-        }
+		public void SetDayText(string s)
+		{
+			day.text = s;
+		}
 
-        public void SetIcon(string s)
-        {
-            icon.text = s;
+		public void SetIcon(string s)
+		{
+			icon.text = s;
 
-            // Fonts can get out of line
-            float y = outOfLineCharacters.Contains(icon.text) ? 0f : 0.84f;
-			icon.transform.localPosition = new Vector3(icon.transform.localPosition.x, y, icon.transform.localPosition.z);
-        }
+			// Fonts can get out of line
+			icon.transform.localPosition = GetPositionFor(s);
+		}
 
-        public void SetTemperatureText(string s)
-        {
-            temperature.text = s;
-        }
+		public void SetTemperatureText(string s)
+		{
+			temperature.text = s;
+		}
 
-        public void SetIconColour(Color colour)
-        {
-            icon.color = colour;
-        }
+		public void SetIconColour(Color colour)
+		{
+			icon.color = colour;
+		}
 
-        public void SetDayColour(Color colour)
-        {
-            day.color = colour;
-        }
+		public void SetDayColour(Color colour)
+		{
+			day.color = colour;
+		}
 
-        public void SetTemperatureTextColour(Color colour)
-        {
-            temperature.color = colour;
-        }
-    }
+		public void SetTemperatureTextColour(Color colour)
+		{
+			temperature.color = colour;
+		}
+
+		private Vector2 GetPositionFor(string iconCode)
+		{
+			switch (iconCode)
+			{
+				case "1":
+					return new Vector2(0.19f, -0.57f);
+				case "3":
+					return new Vector2(0.44f, 0f);
+				case "6":
+					return new Vector2(0.2f, -0.4f);
+				case "A":
+					return new Vector2(0.63801f, -0.00015625f);
+				case "c":
+					return new Vector2(-0.16f, 0.36f);
+				case "K":
+					return new Vector2(0.2f, 0.52f);
+				case "W":
+					return new Vector2(0.2f, 0.52f);
+				case "I":
+					return new Vector2(0.2f, 0.52f);
+				case ",":
+					return new Vector2(0.36f, 0f);
+				case "…":
+					return new Vector2(0.2f, 0f);
+				default:
+					return Vector2.zero;
+			}
+		}
+	}
 }
