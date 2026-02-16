edee0262dc7a1bcd3b65f0546eb87d009394510d
Tom Hewitt <tommhewitt@gmail.com>
2026-02-16 22:28:24 +0000

    add gridstack

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,40 @@
-import './index.scss';
+import { useEffect } from 'react';
+import { GridStack } from 'gridstack';
 
-const App = () => (
-  <div className='app'>Hello world!</div>
-);
+import './index.scss';
+import 'gridstack/dist/gridstack.min.css';
+
+const App = () => {
+  useEffect(() => {
+    GridStack.init();
+  }, []);
+
+  return (
+    <div className='app'>
+
+      <div className='grid-stack'>
+        <div
+          className='grid-stack-item'
+          style={{
+            cursor: 'pointer', 
+          }}
+        >
+          <div className='grid-stack-item-content'>Item 1</div>
+        </div>
+
+        <div
+          className='grid-stack-item'
+          gs-w='2'
+          style={{
+            cursor: 'pointer', 
+          }}
+        >
+          <div className='grid-stack-item-content'>Item 2 wider</div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
-import './index.css';
+import './index.scss';
 
 const container = document.getElementById('root');
 
