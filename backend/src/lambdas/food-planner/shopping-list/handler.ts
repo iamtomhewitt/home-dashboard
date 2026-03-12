4571cdbd236aeee97a985a3e72ac3ef955633ae8
Tom Hewitt <tommhewitt@gmail.com>
2026-03-12 09:17:52 +0000

    populate shopping list

--- backend/src/lambdas/food-planner/shopping-list/handler.ts
+++ backend/src/lambdas/food-planner/shopping-list/handler.ts
@@ -8,6 +8,7 @@ const main = async (e: APIGatewayProxyEvent) => {
   const { id } = e.queryStringParameters || {};
   const bucketName = 'home-dashboard-food-planner';
   const plannerKey = `${id}/planner`;
+  const cookbookKey = `${id}/cookbook`;
 
   if (!id) {
     throw new BadRequestError('Missing \'id\'');
@@ -16,9 +17,55 @@ const main = async (e: APIGatewayProxyEvent) => {
   switch (e.httpMethod) {
     case 'GET': {
       const planner = await s3.getObjectAsJson(bucketName, plannerKey);
-      return response.json(200, 'Success', {
-        planner, 
+      const cookbook = await s3.getObjectAsJson(bucketName, cookbookKey);
+      const recipes = Object.values(planner);
+      const allIngredients = recipes.flatMap((recipe: any) => {
+        const matchingRecipeFromCookbook = cookbook.find((cookbookRecipe: any) => cookbookRecipe.name === recipe);
+        return matchingRecipeFromCookbook ? matchingRecipeFromCookbook.ingredients : [];
       });
+
+      const groupedIngredients = Object.values(
+        allIngredients.reduce((acc, item) => {
+          const key = `${item.name.trim()}_${item.weight}`;
+
+          if (!acc[key]) {
+            acc[key] = {
+              ...item,
+              name: item.name.trim(),
+              amount: 0,
+            };
+          }
+
+          acc[key].amount += Number(item.amount);
+
+          return acc;
+        }, {}),
+      );
+
+      const shoppingList = groupedIngredients.map((ingredient: any) => {
+        let amountSuffix = ingredient.weight;
+
+        switch (ingredient.weight) {
+          case 'quantity':
+            amountSuffix = '';
+            break;
+          case 'grams':
+            amountSuffix = 'g of';
+            break;
+          case 'teaspoon':
+            amountSuffix = 'tsp of';
+            break;
+          case 'tablespoon':
+            amountSuffix = 'tbsp of';
+            break;
+          default:
+            break;
+        }
+
+        return `${ingredient.amount}${amountSuffix} ${ingredient.name}`;
+      });
+
+      return response.json(200, 'Success', shoppingList);
     }
 
     default:
