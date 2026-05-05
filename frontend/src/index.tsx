d14a8c11a89182c70a57c37477b4b5e9dced7213
Tom Hewitt <tommhewitt@gmail.com>
2026-05-05 07:58:57 +0100

    fix: reimplement network logs

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import * as dateFns from 'date-fns';
 import { BrowserRouter } from 'react-router';
 import { createRoot } from 'react-dom/client';
 
@@ -11,6 +12,22 @@ const container = document.getElementById('root');
 if (!container) {
   throw new Error('No container!');
 }
+
+const originalFetch = window.fetch;
+
+window.fetch = async (...args) => {
+  const [url, options] = args;
+  const response = await originalFetch(...args);
+
+  if (!response.ok) {
+    const logs: string[] = JSON.parse(sessionStorage.getItem('logs') || '[]');
+    const formattedUrl = url.toString().includes('amazonaws.com') ? url.toString().split('amazonaws.com')[1] : url;
+    logs.push(`${dateFns.format(new Date(), 'dd/MM/yy HH:mm:ssa')} | ${options?.method} | ${response.status} | ${formattedUrl}`);
+    sessionStorage.setItem('logs', JSON.stringify(logs));
+  }
+
+  return response;
+};
 
 const root = createRoot(container);
 root.render(
